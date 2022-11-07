399ceb17ea6bb6a2a7fbb818fbb1a0091819e532
Александр Флягин <globallll007@gmail.com>
2022-11-07 04:06:26 +0300

    Release

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const config = {
     token: 'BNB',
     network: 'BSC', // if an invalid value is entered, you will get a list of available networks
-    amount: 0.001,
+    amount: 0.001, // if you want to get random amounts, slightly increase this value
     randomizeAmount: true,  // if true, the final amount will be slightly less for a percent below
     spread: 0.5, // in percents, the final amount will be less for random percent up to this value, increase this value for a cheap coins to get more random values
     delay: { min: 5, max: 20 },  // in seconds
